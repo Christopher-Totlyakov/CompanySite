647565f50b10fde2434f792604bc16b312f8b8f7
Christopher Totlyakov <laminex0622@gmail.com>
2024-12-29 20:43:32 +0200

    first commit

---
+++ assets/js/loadContent.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/_includes/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
+
+    fetch("/_includes/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+
+    
+});
+function loadPage(page) {
+fetch(page)
+.then(response => response.text())
+.then(data => {
+    document.getElementById("main-content").innerHTML = data;
+})
+.catch(error => console.error("Error loading page:", error));
+}
