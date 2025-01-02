d11f8dcd6be425d92ec4dffe50fb65dc52bc10da
Christopher Totlyakov <laminex0622@gmail.com>
2025-01-02 12:01:28 +0200

    finish services page

--- assets/js/servicesInfo.js
+++ assets/js/servicesInfo.js
@@ -1,9 +1,13 @@
 function servicesInfo(button) {
     const popup = document.getElementById('popup');
+    const popupTitle = document.getElementById('popup-title');
     const popupText = document.getElementById('popup-text');
     const closeBtn = document.getElementById('close-btn');
 
+    const title = button.getAttribute('data-title');
     const info = button.getAttribute('data-info');
+
+    popupTitle.textContent = title; 
     popupText.textContent = info; 
     popup.style.display = 'flex'; 
 
